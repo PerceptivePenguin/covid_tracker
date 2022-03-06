4f9afeb682a8ac5fd0e2976122041988bfab7463
pendragon35 <kevinpendragon35@163.com>
2022-03-07 01:48:05 +0800

    Initial commit

---
+++ src/test/java/io/perceptivepenguin/covidtracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.perceptivepenguin.covidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
