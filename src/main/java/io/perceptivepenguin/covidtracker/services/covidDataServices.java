779cd9892214caa9195a5e2c193bee1058b78438
perceptivepenguin <perceptivepenguin745@gmail.com>
2022-03-08 20:16:32 +0800

    add csvRecord and csvParse features

--- src/main/java/io/perceptivepenguin/covidtracker/services/covidDataServices.java
+++ src/main/java/io/perceptivepenguin/covidtracker/services/covidDataServices.java
@@ -1,17 +1,36 @@
 package io.perceptivepenguin.covidtracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+@Service
 public class covidDataServices {
 
-    private static String VIRUS_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
 
-    public void fetchVirusData(){
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().
                 uri(URI.create(VIRUS_DATA_URL))
                 .build();
+        HttpResponse<String> httpResponse =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String id = record.get("ID");
+            String customerNo = record.get("CustomerNo");
+            String name = record.get("Name");
+        }
     }
 }
